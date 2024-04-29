048ee4b007447ddf7498e0485da2d025973728cb
YIYAO1019 <127611463+YIYAO1019@users.noreply.github.com>
2024-04-29 23:35:24 +0800

    feat: Route HR screens

--- src/app/platform/components/navigation/router.jsx
+++ src/app/platform/components/navigation/router.jsx
@@ -1,11 +1,13 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import CandidateLayout from '../Layouts/CandidateLayout';
+import HrLayout from '../Layouts/HrLayout';
+import Landing from '../../../modules/landing/Landing';
 import CareersScreens from '../../../modules/candidate/Careers/Navigation/CareersScreens';
 import FeedbackScreens from '../../../modules/candidate/Feedback/Navigation/FeedbackScreens';
 import OnboardingScreens from '../../../modules/candidate/Onboarding/Navigation/OnboardingScreens';
 import StatusScreens from '../../../modules/candidate/Status/Navigation/StatusScreens';
-import Landing from '../../../modules/landing/Landing';
-// import Careers from '../../../modules/candidate/Careers/Screens/Careers';
+import CandidatesScreens from '../../../modules/hr/Candidates/Navigation/CandidatesScreens';
+import AnalyticsScreens from '../../../modules/hr/Analytics/Navigation/AnalyticsScreens';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -20,7 +22,10 @@ const router = createBrowserRouter(
                 {OnboardingScreens}
                 {StatusScreens}
             </Route>
-            ,
+            <Route element={<HrLayout />}>
+                {CandidatesScreens}
+                {AnalyticsScreens}
+            </Route>
         </>,
     ),
 );
