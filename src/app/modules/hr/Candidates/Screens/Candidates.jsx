2ac092a2656dec1b6af52fe91a0bee1ce31173bf
YIYAO1019 <127611463+YIYAO1019@users.noreply.github.com>
2024-05-01 21:37:36 +0800

    feat: Added search field in Candidates page (HR view)

--- src/app/modules/hr/Candidates/Screens/Candidates.jsx
+++ src/app/modules/hr/Candidates/Screens/Candidates.jsx
@@ -1,3 +1,27 @@
+import SearchField from '../Components/SearchField.jsx';
+import styled from 'styled-components';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+
 export default function Candidates() {
-    return <p>Testing candidates</p>;
+    const StyledDiv = styled.div`
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    `;
+
+    return (
+        <StyledDiv>
+            <SearchField />
+            <Card>
+                <CardMedia>
+
+                </CardMedia>
+                <CardContent>
+
+                </CardMedia>
+            </Card>
+        </StyledDiv>
+    );
 }
