66161552a49359801ccb49fad31c20beab3d4077
wongjie <melodywj@yahoo.com>
2024-05-01 11:47:20 +0800

    ONBOARDING CODE

--- src/app/modules/candidate/Onboarding/Screens/Onboarding.jsx
+++ src/app/modules/candidate/Onboarding/Screens/Onboarding.jsx
@@ -1,3 +1,16 @@
+import styled from 'styled-components';
+import FONTWEIGHT from '../../../../platform/Style/FontWeight';
+import FONTSIZE from '../../../../platform/Style/FontSize';
+
+const StyledTitle = styled.h1`
+    font-weight: ${FONTWEIGHT.BOLD};
+    font-size: ${FONTSIZE.large};
+`;
+
 export default function Onboarding() {
-    return <p>Testing Onboarding</p>;
+    return (
+        <div>
+            <StyledTitle>Welcome, Vinnie!</StyledTitle>
+        </div>
+    );
 }
