e6c04f5f2e1a0b156342796d19033c0d6313f7cf
VinnieTiang <117924139+VinnieTiang@users.noreply.github.com>
2024-05-09 01:20:52 +0800

    Added interview and report screen

--- src/app/modules/hr/Candidates/Navigation/CandidatesScreens.jsx
+++ src/app/modules/hr/Candidates/Navigation/CandidatesScreens.jsx
@@ -1,13 +1,28 @@
 import { Route } from 'react-router-dom';
 import Candidates from '../Screens/Candidates';
-// import Vinnie from '../Screens/Vinnie';
+import Vinnie from '../Screens/Vinnie';
+import VinnieInterview from '../Screens/VinnieInterview';
+import VinnieReport from '../Screens/VinnieReport';
 
 const CandidatesScreens = (
     <>
         <Route
             path="/hr/candidates"
-            element={<Candidates />}
-            // element={<Vinnie />}
+            // element={<Candidates />}
+            element={<Vinnie />}
+        />
+
+        <Route
+            path="/hr/candidate/Vinnie"
+            element={<Vinnie />}
+        />
+        <Route
+            path="/hr/candidate/interview"
+            element={<VinnieInterview />}
+        />
+        <Route
+            path="/hr/candidate/report"
+            element={<VinnieReport />}
         />
     </>
 );
---
+++ src/app/modules/hr/Candidates/Screens/VinnieReport.jsx
@@ -0,0 +1,138 @@
+import FONTWEIGHT from '../../../../platform/Style/FontWeight';
+import styled from 'styled-components';
+import { assets } from '../../../../platform/assets/assets';
+import { Button } from '@mui/material';
+import PlatformReusableStyles from '../../../../platform/Style/PlatformReusableStyles';
+import { Link } from 'react-router-dom';
+
+const StyledText = styled.p`
+    font-weight: ${FONTWEIGHT.BOLD};
+    font-size: 30px;
+`;
+
+const StyledDiv = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 300;
+`;
+const StyledImage = styled.img`
+    margin-right: 20px;
+`;
+
+const Spacing = styled.div`
+    margin-top: 80px;
+`;
+
+const StyledIcon = styled.img`
+    margin-right: 5px;
+    width: 25px;
+    height: 25px;
+`;
+
+const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const HorizontalTextWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+const SubText = styled.p`
+    margin-top: 10px;
+    display: inline-block;
+`;
+
+const HorizontalSpacing = styled.div`
+    margin-right: 50px;
+`;
+
+const ButtonsDiv = styled.div`
+    display: flex;
+    flex-grow: 1;
+    gap: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+`;
+
+const StyledButton = styled(Button)`
+    flex-grow: 1;
+`;
+
+const StyledTitle = styled.p`
+    color: white;
+    font-weight: ${FONTWEIGHT.BOLD};
+`;
+
+const StyledWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-right: 50px;
+`;
+
+const StyledContainer = styled.div`
+    background-color: black;
+    padding: 10px;
+    display: inline-block;
+    border-radius: 8px;
+    margin-top: 10px;
+`;
+
+const StyledInfoTitle = styled.p`
+    font-weight: ${FONTWEIGHT.BOLD};
+    font-size: 15px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-right: 10px;
+`;
+
+const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export default function VinnieReport() {
+    return (
+        <div>
+            <StyledDiv>
+                <StyledImage
+                    src={assets.vinnie_photo}
+                    alt=""
+                />
+                <TextWrapper>
+                    <StyledText>Vinnie Tiang Wen Ying</StyledText>
+                    <SubText>UI/UX Designer</SubText>
+                    <Spacing></Spacing>
+                    <HorizontalTextWrapper>
+                        <StyledIcon
+                            src={assets.profile_phone}
+                            alt=""
+                        />
+
+                        <SubText>+601155093081</SubText>
+                        <HorizontalSpacing></HorizontalSpacing>
+                        <StyledIcon
+                            src={assets.profile_email}
+                            alt=""
+                        />
+
+                        <SubText>vinnieying223@gmail.com</SubText>
+                    </HorizontalTextWrapper>
+                </TextWrapper>
+            </StyledDiv>
+            <StyledDiv>
+                <ButtonsDiv>
+                    <Link to="/hr/candidates">
+                        <StyledButton style={PlatformReusableStyles.BlackOutlineButtonStyles}>Summary</StyledButton>
+                    </Link>
+                    <Link to="/hr/candidate/interview">
+                        <StyledButton style={PlatformReusableStyles.BlackOutlineButtonStyles}>Interview</StyledButton>
+                    </Link>
+                    <Link to="/hr/candidate/report">
+                        <StyledButton style={PlatformReusableStyles.PrimaryButtonStyles}>AI Reports</StyledButton>
+                    </Link>
+                </ButtonsDiv>
+            </StyledDiv>
+        </div>
+    );
+}
