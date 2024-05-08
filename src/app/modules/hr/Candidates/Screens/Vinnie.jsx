57688e2639c33085a77fa34b956730f5d5673e25
YIYAO1019 <127611463+YIYAO1019@users.noreply.github.com>
2024-05-08 16:59:18 +0800

    feat(HR Candidate): add filter dropdown menu to filter candidates

--- src/app/modules/hr/Candidates/Screens/Vinnie.jsx
+++ src/app/modules/hr/Candidates/Screens/Vinnie.jsx
@@ -84,11 +84,11 @@ const StyledInfoTitle = styled.p`
     margin-bottom: 10px;
     margin-right: 10px;
 `;
-const StyledGrid = styled.div`
-    display: grid;
-    grid-template-columns: max-content 1fr;
-    gap: 10px;
-`;
+// const StyledGrid = styled.div`
+//     display: grid;
+//     grid-template-columns: max-content 1fr;
+//     gap: 10px;
+// `;
 
 const InfoWrapper = styled.div`
     display: flex;
