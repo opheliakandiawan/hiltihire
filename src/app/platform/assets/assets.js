d7e2c4a71a398f35b496e38323505d58485a248e
wongjie <melodywj@yahoo.com>
2024-05-10 01:14:01 +0800

    Onboarding done

--- src/app/platform/assets/assets.js
+++ src/app/platform/assets/assets.js
@@ -1,7 +1,17 @@
 import vinnie_photo from './vinnie-photo.png';
 import vinnie_horizontal from './vinnie-horizontal.jpeg';
+import Onboardingdecopic from './Onboardingdecopic.jpg';
+import Onboarding1 from './Onboarding1.png';
+import Onboarding2 from './Onboarding2.png';
+import Onboarding3 from './Onboarding3.png';
+import Onboarding4 from './Onboarding4.png';
 
 export const assets = {
     vinnie_photo,
     vinnie_horizontal,
+    Onboardingdecopic,
+    Onboarding1,
+    Onboarding2,
+    Onboarding3,
+    Onboarding4,
 };
